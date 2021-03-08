f31c91652d4ec20cf7c8772f58d7c4fbb009e8ad
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-08 12:03:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,48 +1,28 @@
 class CashRegister
-  attr_accessor :total, :price, :discount, :items, :quantity
 
-  def initialize(discount = 0)
-    @total = 0
-    @price = price
-    @quantity = quantity
-    @discount = discount
-    @items = []
-    @transactions = []
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount = nil)
+    self.total = 0
+    self.discount = discount
+    self.items = []
   end
 
-  def total
-    @total
-  end
-
-  def add_item(item, price, quantity = 1)
-    @total += (price * quantity)
-    @price = price
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
-    end
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times { self.items << title }
+    self.last_transaction = [title, price, quantity]
   end
 
   def apply_discount
-    if @discount > 0
-    self.total -= (0.01 * @discount * @total).to_i
-    "After the discount, the total comes to $#{self.total}."
-    else
-    "There is no discount to apply."
-  end
+    self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total = self.total * 8 / 10 }."
   end
 
-  def items
-    @items
+  def void_last_transaction
+    self.total -= self.last_transaction[1] * self.last_transaction[2]
+    self.last_transaction[2].times do
+      self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
+    end
   end
-
-def void_last_transaction
-  @total -= @price
-end
 
 end
